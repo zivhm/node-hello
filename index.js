c7ff3bb72af10fcac1fb9dc83893d3d824f9dc8f
lidorlg <lidorlg@gmail.com>
2021-08-19 19:32:32 +0300

    new version

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Lidor Container!\n'
+  const msg = 'Hello Lidor new version!\n'
   res.end(msg);
 });
 
